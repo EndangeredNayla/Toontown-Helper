dc6f5e32c395ad18945c7f5c64bb007ee7867295
Tyler Royer <troyer@bgsu.edu>
2019-01-01 14:52:42 -0500

    Added panel link for Walrus Way.

--- src/com/tylerroyer/ttr_helper/maps/WorldMapPanel.java
+++ src/com/tylerroyer/ttr_helper/maps/WorldMapPanel.java
@@ -67,6 +67,9 @@ public class WorldMapPanel extends MapPanel {
 		Shape tenorTerraceShape = HelperFunctions.createPolygonFromPoints(
 				new int[] { 376, 361, 329, 296, 360, 348, 273, 333, 364, 376 },
 				new int[] { 230, 230, 257, 257, 203, 201, 266, 267, 239, 239 });
+		Shape walrusWayShape = HelperFunctions.createPolygonFromPoints(
+				new int[] { 682, 682, 636, 655, 668, 652, 695, 695 },
+				new int[] { 272, 266, 266, 236, 236, 257, 261, 272 });
 
 		// TODO Ideally, the tab indices wouldn't be hard-coded. But for now, it should
 		// be fine unless TTR adds more streets.
@@ -82,6 +85,7 @@ public class WorldMapPanel extends MapPanel {
 				new PanelLink(oakStreetShape, missingAsset, 9),
 				new PanelLink(altoAvenueShape, missingAsset, 10),
 				new PanelLink(baritoneBoulevardShape, missingAsset, 11),
-				new PanelLink(tenorTerraceShape, missingAsset, 12)};
+				new PanelLink(tenorTerraceShape, missingAsset, 12),
+				new PanelLink(walrusWayShape, missingAsset, 15)};
 	}
 }
